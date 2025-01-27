25dbac2ad6e49a3b3590772cb7742a2718c4b16c
Diego Paredes <95343307+diego19p@users.noreply.github.com>
2025-01-27 10:41:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var scrollDownLink = document.querySelector(".scroll-down");
-    var socialLinks = document.querySelector(".social-links");
-
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollDownLink = document.querySelector(".scroll-down");
+    const socialLinks = document.querySelector(".social-links");
+  
     if (scrollDownLink) {
-        scrollDownLink.addEventListener("click", function(event) {
-            event.preventDefault();
-            var aboutSection = document.querySelector("#about");
-
-            if (aboutSection) {
-                aboutSection.scrollIntoView({ behavior: "smooth" });
-                socialLinks.classList.remove("social-links-hidden");
-            }
-        });
+      scrollDownLink.addEventListener("click", function (event) {
+        event.preventDefault();
+        const aboutSection = document.querySelector("#about");
+  
+        if (aboutSection) {
+          aboutSection.scrollIntoView({ behavior: "smooth" });
+          socialLinks.classList.remove("social-links-hidden");
+        }
+      });
     }
-});
+  
+    window.addEventListener("error", function (event) {
+      console.error("Error:", event.message);
+    });
+  });
